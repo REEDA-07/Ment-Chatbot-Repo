090814584ac7f4f4fb45f98d34136aa6c40832f1
REEDA-07 <162997378+REEDA-07@users.noreply.github.com>
2024-03-26 17:45:46 +0100

    Update mentchat.py

--- mentchat.py
+++ mentchat.py
@@ -96,27 +96,35 @@ with chat_response:
         chat_response.write(f"\nChatbot: {response}")
 
  # history starting 
- #        hist_list.append(user_word)
+        hist_list.append(user_word)
 
 # # Save the history of the texts 
-# with open('history.txt', 'a') as file:
-#         for item in hist_list:
-#             file.write(str(item) + '\n')
-#             file.write(response)    
+with open('history.txt', 'a') as file:
+        for item in hist_list:
+            file.write(str(item) + '\n')
+            file.write(response)    
 
-# import csv
-# files = 'history.txt'
-# with open(files) as f:
-#   reader = csv.reader(f)
-#   data = list(reader)
+import csv
+files = 'history.txt'
+try:
+    with open(files, encoding='utf-8-sig') as f:
+        reader = csv.reader(f)
+        data_hist = list(reader)
+except UnicodeDecodeError:
+    with open(files, encoding='latin-1') as f:
+        reader = csv.reader(f)
+        data_hist = list(reader)
+        
+history = pd.DataFrame(data_hist)
+st.sidebar.subheader('Chat History', divider=True)
+st.sidebar.write(history)
+history = pd.Series(data)
+st.sidebar.subheader('Chat History', divider = True)
+st.sidebar.write(history)
 
-# history = pd.Series(data)
-# st.sidebar.subheader('Chat History', divider = True)
-# st.sidebar.write(history)
+st.header('Project Background Information', divider = True)
+st.write("The objectives of a mental health chatbot include providing supportive conversations, offering immediate assistance in crisis situations, normalizing mental health discussions, assessing and monitoring mental health, providing psychoeducation and coping strategies, facilitating access to resources, maintaining user privacy, continuously improving through feedback, and collaborating with mental health professionals to enhance support effectiveness.")
 
-# st.header('Project Background Information', divider = True)
-# st.write("The objectives of a mental health chatbot include providing supportive conversations, offering immediate assistance in crisis situations, normalizing mental health discussions, assessing and monitoring mental health, providing psychoeducation and coping strategies, facilitating access to resources, maintaining user privacy, continuously improving through feedback, and collaborating with mental health professionals to enhance support effectiveness.")
-
-# st.markdown("<br>", unsafe_allow_html= True)
-# st.markdown("<br>", unsafe_allow_html= True)
+st.markdown("<br>", unsafe_allow_html= True)
+st.markdown("<br>", unsafe_allow_html= True)
 
